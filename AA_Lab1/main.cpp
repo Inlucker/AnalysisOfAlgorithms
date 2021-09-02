8b93baf36b92164a19636716eb7126e011ea701a
Inlucker <Arseny707@mail.ru>
2021-09-02 23:43:08 +0300

    added 3 time tests

--- AA_Lab1/main.cpp
+++ AA_Lab1/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
 //#include "getCPUTime.h"
+#include <ctime>
 #include <Windows.h>
+#include <chrono>
 
 using namespace std;
 
@@ -23,97 +25,21 @@ ostream& operator <<(ostream& os, const vector<size_t>& vec);
     }
     return -1;        // Failed.
 }*/
-
-/*
- * Author:  David Robert Nadeau
- * Site:    http://NadeauSoftware.com/
- * License: Creative Commons Attribution 3.0 Unported License
- *          http://creativecommons.org/licenses/by/3.0/deed.en_US
- */
-#if defined(_WIN32)
-#include <Windows.h>
-
-#elif defined(__unix__) || defined(__unix) || defined(unix) || (defined(__APPLE__) && defined(__MACH__))
-#include <unistd.h>
-#include <sys/resource.h>
-#include <sys/times.h>
-#include <time.h>
-
-#else
-#error "Unable to define getCPUTime( ) for an unknown OS."
-#endif
-
-
-/**
- * Returns the amount of CPU time used by the current process,
- * in seconds, or -1.0 if an error occurred.
- */
-double getCPUTime() {
-#if defined(_WIN32)
-    /* Windows -------------------------------------------------- */
+double getCPUTime()
+{
     FILETIME createTime;
     FILETIME exitTime;
     FILETIME kernelTime;
     FILETIME userTime;
-    if (GetProcessTimes(GetCurrentProcess(), &createTime, &exitTime, &kernelTime, &userTime) != -1) {
-        ULARGE_INTEGER li = {{userTime.dwLowDateTime, userTime.dwHighDateTime }};
-        return li.QuadPart / 10000000.;
-    }
-
-#elif defined(__unix__) || defined(__unix) || defined(unix) || (defined(__APPLE__) && defined(__MACH__))
-    /* AIX, BSD, Cygwin, HP-UX, Linux, OSX, and Solaris --------- */
-
-#if defined(_POSIX_TIMERS) && (_POSIX_TIMERS > 0)
-    /* Prefer high-res POSIX timers, when available. */
+    if (GetProcessTimes( GetCurrentProcess( ), &createTime, &exitTime, &kernelTime, &userTime ) != -1)
     {
-        clockid_t id;
-        struct timespec ts;
-#if _POSIX_CPUTIME > 0
-        /* Clock ids vary by OS.  Query the id, if possible. */
-        if (clock_getcpuclockid(0, &id) == -1)
-#endif
-#if defined(CLOCK_PROCESS_CPUTIME_ID)
-            /* Use known clock id for AIX, Linux, or Solaris. */
-            id = CLOCK_PROCESS_CPUTIME_ID;
-#elif defined(CLOCK_VIRTUAL)
-        /* Use known clock id for BSD or HP-UX. */
-        id = CLOCK_VIRTUAL;
-#else
-        id = (clockid_t) - 1;
-#endif
-        if (id != (clockid_t) - 1 && clock_gettime(id, &ts) != -1)
-            return (double) ts.tv_sec + (double) ts.tv_nsec / 1000000000.0;
+        SYSTEMTIME userSystemTime;
+        if ( FileTimeToSystemTime( &userTime, &userSystemTime ) != -1 )
+            return (double)userSystemTime.wHour * 3600.0 +
+                (double)userSystemTime.wMinute * 60.0 +
+                (double)userSystemTime.wSecond +
+                (double)userSystemTime.wMilliseconds / 1000.0;
     }
-#endif
-
-#if defined(RUSAGE_SELF)
-    {
-        struct rusage rusage;
-        if (getrusage(RUSAGE_SELF, &rusage) != -1)
-            return (double) rusage.ru_utime.tv_sec + (double) rusage.ru_utime.tv_usec / 1000000.0;
-    }
-#endif
-
-#if defined(_SC_CLK_TCK)
-    {
-        const double ticks = (double) sysconf(_SC_CLK_TCK);
-        struct tms tms;
-        if (times(&tms) != (clock_t) - 1)
-            return (double) tms.tms_utime / ticks;
-    }
-#endif
-
-#if defined(CLOCKS_PER_SEC)
-    {
-        clock_t cl = clock();
-        if (cl != (clock_t) - 1)
-            return (double) cl / (double) CLOCKS_PER_SEC;
-    }
-#endif
-
-#endif
-
-    return -1;        /* Failed. */
 }
 
 int main()
@@ -124,13 +50,28 @@ int main()
 
     double startTime, endTime;
 
-    startTime = getCPUTime( );
+    auto t_start = std::chrono::high_resolution_clock::now();
+    clock_t start = clock();
+    startTime = getCPUTime();
+
     for (int i = 0; i <= ITERATIONS; i++)
         rez = LevLen(str1, str2);
-    endTime = getCPUTime( );
 
-    fprintf( stderr, "CPU time used = %lf\n", (endTime - startTime) );
-    fprintf( stderr, "CPU time of LevLen = %lf\n", (endTime - startTime)/ITERATIONS );
+    auto t_end = std::chrono::high_resolution_clock::now();
+    endTime = getCPUTime();
+    clock_t end = clock();
+
+    clock_t res  = (end-start);
+    double d_res = double(res)/CLOCKS_PER_SEC;
+
+    cout << "Chrono full time = " << std::chrono::duration<double, std::milli>(t_end-t_start).count()/CLOCKS_PER_SEC << endl;
+    cout << "Chrono time of LevLen = " << std::chrono::duration<double, std::milli>(t_end-t_start).count()/CLOCKS_PER_SEC/ITERATIONS << endl;
+
+    cout << "Clock full time = " << d_res << endl;
+    cout << "Clock time of LevLen = " << d_res/ITERATIONS << endl;
+
+    fprintf( stderr, "CPU time used = %lf\n", (endTime - startTime));
+    fprintf( stderr, "CPU time of LevLen = %lf\n", (endTime - startTime)/ITERATIONS);
 
 
     cout << "length between \"" << str1 << "\" and \"" << str2 << "\" = " << rez << endl;
