7219802f39f1c28bcbde7f56ee4fafbb5a17a45f
Inlucker <arseny707@mail.ru>
2021-09-05 16:30:50 +0300

    LevLenRec

--- AA_Lab1/main.cpp
+++ AA_Lab1/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <vector>
-//#include "getCPUTime.h"
+
+#include <chrono>
 #include <ctime>
 #include <Windows.h>
-#include <chrono>
 
 using namespace std;
 
-#define ITERATIONS 1000000
-
 size_t LevLen(string src, string dst);
+
+size_t LevLenRec(string str1, string str2);
 
 ostream& operator <<(ostream& os, const vector<size_t>& vec);
 
@@ -25,6 +25,7 @@ ostream& operator <<(ostream& os, const vector<size_t>& vec);
     }
     return -1;        // Failed.
 }*/
+
 double getCPUTime()
 {
     FILETIME createTime;
@@ -42,27 +43,28 @@ double getCPUTime()
     }
 }
 
+#define ITERATIONS 1
+
 int main()
 {
     string str1 = "arestant";
     string str2 = "dagestan";
     size_t rez = 0;
 
-    double startTime, endTime;
-
     auto t_start = std::chrono::high_resolution_clock::now();
     clock_t start = clock();
-    startTime = getCPUTime();
+    double startTime = getCPUTime();
 
     for (int i = 0; i <= ITERATIONS; i++)
-        rez = LevLen(str1, str2);
+        rez = LevLenRec(str1, str2);
 
     auto t_end = std::chrono::high_resolution_clock::now();
-    endTime = getCPUTime();
     clock_t end = clock();
+    double endTime = getCPUTime();
 
     clock_t res  = (end-start);
     double d_res = double(res)/CLOCKS_PER_SEC;
+    double cpu_time = (endTime - startTime);
 
     cout << "Chrono full time = " << std::chrono::duration<double, std::milli>(t_end-t_start).count()/CLOCKS_PER_SEC << endl;
     cout << "Chrono time of LevLen = " << std::chrono::duration<double, std::milli>(t_end-t_start).count()/CLOCKS_PER_SEC/ITERATIONS << endl;
@@ -70,8 +72,10 @@ int main()
     cout << "Clock full time = " << d_res << endl;
     cout << "Clock time of LevLen = " << d_res/ITERATIONS << endl;
 
-    fprintf( stderr, "CPU time used = %lf\n", (endTime - startTime));
-    fprintf( stderr, "CPU time of LevLen = %lf\n", (endTime - startTime)/ITERATIONS);
+    cout << "CPU time used = " << cpu_time << endl;
+    cout << "CPU time of LevLen = " << cpu_time/ITERATIONS << endl;
+    //fprintf( stderr, "CPU time used = %lf\n", cpu_time);
+    //fprintf( stderr, "CPU time of LevLen = %lf\n", cpu_time/ITERATIONS);
 
 
     cout << "length between \"" << str1 << "\" and \"" << str2 << "\" = " << rez << endl;
@@ -126,6 +130,33 @@ size_t LevLen(string src, string dst)
     }
 
     return cur_row[min_size];
+}
+
+size_t LevLenRec(string str1, string str2)
+{
+    size_t i = str1.length();
+    size_t j = str2.length();
+    if (i == 0 && j == 0)
+    {
+        return 0;
+    }
+    else if (i > 0 && j == 0)
+    {
+        return i;
+    }
+    else if (i == 0 && j > 0)
+    {
+        return j;
+    }
+    else
+    {
+        size_t add = LevLenRec(str1.substr(0, i), str2.substr(0, j-1)) + 1;
+        size_t del = LevLenRec(str1.substr(0, i-1), str2.substr(0, j)) + 1;
+        size_t change = LevLenRec(str1.substr(0, i-1), str2.substr(0, j-1));
+        if (str1[i-1] != str2[j-1])
+            change++;
+        return  min(add, min(del, change));
+    }
 }
 
 ostream& operator <<(ostream& os, const vector<size_t>& vec)
