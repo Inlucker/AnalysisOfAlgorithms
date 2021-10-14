4e4ddbd1f06c57f3d0207ab16a1231e30beece6f
Inlucker <Arseny707@mail.ru>
2021-10-14 14:18:41 +0300

    added functests

--- AA_Lab3/main.cpp
+++ AA_Lab3/main.cpp
@@ -11,6 +11,88 @@ using namespace std;
 
 void printMas(const int *mas, int size);
 
+void funcTests()
+{
+    int n = 1;
+    int mas1[1] = {10};
+    cout << "mas1 = ";
+    printMas(mas1, 1);
+    BubbleSort(&mas1[0], &mas1[n-1]);
+    cout << "sorted1 by BubbleSort = ";
+    printMas(mas1, 1);
+    bool flag = true;
+    if (mas1[0] != 10)
+        flag = false;
+    if (flag)
+        cout << "BbubleSort() with mas1 is correct!" << endl;
+    else
+        cout << "BbubleSort() with mas1 is NOT correct!" << endl;
+
+    SelectionSort(&mas1[0], &mas1[n-1]);
+    cout << "sorted1 by SelectionSort = ";
+    printMas(mas1, 1);
+    flag = true;
+    if (mas1[0] != 10)
+        flag = false;
+    if (flag)
+        cout << "SelectionSort() with mas1 is correct!" << endl;
+    else
+        cout << "SelectionSort() with mas1 is NOT correct!" << endl;
+
+    InsertionSort(&mas1[0], &mas1[n-1]);
+    cout << "sorted1 by InsertionSort = ";
+    printMas(mas1, 1);
+    flag = true;
+    if (mas1[0] != 10)
+        flag = false;
+    if (flag)
+        cout << "SelectionSort() with mas1 is correct!" << endl;
+    else
+        cout << "SelectionSort() with mas1 is NOT correct!" << endl;
+
+    n = 10;
+    int mas10[10] = {10,7,9,5,3,1,8,2,4,6};
+    cout << "mas10 = ";
+    printMas(mas10, 10);
+    BubbleSort(&mas10[0], &mas10[n-1]);
+    cout << "sorted10 by BubbleSort = ";
+    printMas(mas10, 10);
+    flag = true;
+    for (int i = 0; i < 10; i++)
+        if (mas10[i] != i+1)
+            flag = false;
+    if (flag)
+        cout << "BbubleSort() with mas10 is correct!" << endl;
+    else
+        cout << "BbubleSort() with mas10 is NOT correct!" << endl;
+
+    int mas10_2[10] = {10,7,9,5,3,1,8,2,4,6};
+    SelectionSort(&mas10_2[0], &mas10_2[n-1]);
+    cout << "sorted10 by SelectionSort = ";
+    printMas(mas10, 10);
+    flag = true;
+    for (int i = 0; i < 10; i++)
+        if (mas10_2[i] != i+1)
+            flag = false;
+    if (flag)
+        cout << "SelectionSort() with mas10 is correct!" << endl;
+    else
+        cout << "SelectionSort() with mas10 is NOT correct!" << endl;
+
+    int mas10_3[10] = {10,7,9,5,3,1,8,2,4,6};
+    InsertionSort(&mas10_3[0], &mas10_3[n-1]);
+    cout << "sorted10 by InsertionSort = ";
+    printMas(mas10, 10);
+    flag = true;
+    for (int i = 0; i < 10; i++)
+        if (mas10_3[i] != i+1)
+            flag = false;
+    if (flag)
+        cout << "InsertionSort() with mas10 is correct!" << endl;
+    else
+        cout << "InsertionSort() with mas10 is NOT correct!" << endl;
+}
+
 int main()
 {
     int mas[N];
@@ -47,12 +129,14 @@ int main()
     //cout << getSelectionSort(&mas[0], &mas[N-1]) << endl;
     //cout << getInsertionSort(&mas[0], &mas[N-1]) << endl;
 
-    getBubbleSortTimes(&genMasRandom);
-    getBubbleSortTimes(&genMasBest);
-    getBubbleSortTimes(&genMasWorst);
+    //getBubbleSortTimes(&genMasRandom);
+    //getBubbleSortTimes(&genMasBest);
+    //getBubbleSortTimes(&genMasWorst);
     //getBubbleSortTimes(&genMasRandom);
     //getSelectionSortTimes(&genMasRandom);
-    //getInsertionSortTimes(&genMasRandom);
+    //getInsertionSortTimes(&genMasRandom);\
+
+    funcTests();
 
     return 0;
 }
