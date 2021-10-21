ff00c30f062dbd0f2bcc2b04007d920c0c19b7f2
Inlucker <arseny707@mail.ru>
2021-10-21 16:45:03 +0300

    startef ot4et for lab2

--- AA_Lab2/report/inc/src/algorithms.cpp
+++ AA_Lab2/report/inc/src/algorithms.cpp
@@ -1,39 +1,3 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-typedef vector<vector<int>> mtrx;
-
-using namespace std;
-
-#define SIZE_ERROR 1;
-
-void mtrxGen(mtrx &rez, int n, int m, int lim)
-{
-    for (int i = 0; i < n; i++)
-    {
-        rez.push_back(vector<int>(m));
-        for (int j = 0; j < m; j++)
-        {
-            rez[i][j] = rand() % lim;
-        }
-    }
-}
-
-int prepare(mtrx &result, int n1, int m1, int n2, int m2)
-{
-    if (m1 != n2)
-    {
-        return SIZE_ERROR;
-    }
-    for (int i = 0; i < n1; i++)
-    {
-        result.push_back(vector<int>(m2, 0));
-    }
-    return 0;
-}
-
 int standartMult(mtrx &rez, mtrx mtrx1, int n1, int m1, mtrx mtrx2, int n2, int m2)
 {
     if (m1 != n2)
@@ -53,7 +17,6 @@ int vinograd(mtrx &rez, mtrx mtrx1, int n1, int m1, mtrx mtrx2, int n2, int m2)
 {
     if (m1 != n2)
         return SIZE_ERROR;
-
     vector<int> mulH(n1, 0);
     for (int i = 0; i < n1; i++)
         for (int j = 0; j < m1 / 2; j++)
@@ -110,46 +73,5 @@ int optimizedVinograd(mtrx &rez, mtrx mtrx1, int n1, int m1, mtrx mtrrx2, int n2
                 rez[i][j] += mtrx1[i][n2 - 1] * mtrrx2[n2 - 1][j];
         }
 
-    return 0;
-}
-
-void printMtrx(mtrx mtr, int n, int m)
-{
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cout << mtr[i][j] << ' ';
-        }
-        cout << endl;
-    }
-    cout << endl;
-}
-
-int main()
-{
-    int n = 3;
-    mtrx mtrx1, mtrx2, rez;
-    mtrxGen(mtrx1, n, n, 5);
-    mtrxGen(mtrx2, n, n, 5);
-    prepare(rez, n, n, n, n);
-
-    cout << "Matrix 1:" << endl;
-    printMtrx(mtrx1, n, n);
-
-    cout << "Matrix 2:" << endl;
-    printMtrx(mtrx2, n, n);
-
-    standartMult(rez, mtrx1, n, n, mtrx2, n, n);
-    cout << "standartMult Result Matrix:" << endl;
-    printMtrx(rez, n, n);
-
-    vinograd(rez, mtrx1, n, n, mtrx2, n, n);
-    cout << "vinograd Result Matrix:" << endl;
-    printMtrx(rez, n, n);
-
-    optimizedVinograd(rez, mtrx1, n, n, mtrx2, n, n);
-    cout << "optimizedVinograd Result Matrix:" << endl;
-    printMtrx(rez, n, n);
     return 0;
 }
